d8d6e89197503530a0693772b8878897559b161b
devgiljong <calf-love@naver.com>
2024-02-20 23:25:07 +0900

    :heavy_plus_sign: Tailwindcss dependency 추가

---
+++ src/@app/root/entry-point.tsx
@@ -0,0 +1,5 @@
+function EntryPoint() {
+  return <div></div>;
+}
+
+export default EntryPoint;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EntryPoint from './@app/root/entry-point.tsx';
+import './@app/style/tailwind.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <EntryPoint />
   </React.StrictMode>,
-)
+);
